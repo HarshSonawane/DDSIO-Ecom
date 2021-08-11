7e31545893fcff5c3e2f0835a42102b2d39e204e
Harshwardhan Sonawane <43908882+HarshSonawane@users.noreply.github.com>
2021-08-11 21:43:00 +0530

    Update views.py

--- shopMart/views.py
+++ shopMart/views.py
@@ -10,4 +10,10 @@ def Details(request,myid):
     all_products = Product.objects.all()
     products=Product.objects.filter(id=myid)
     return render(request,'productDetails.html',{'products':products[0],'all_products':all_products})
+
+
+def product_details(request, id=None):
+    object = Product.objects.get(pk=id)
+    object.get_images()
+    return object
  
